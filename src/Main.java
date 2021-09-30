20156bc252e66af403f571998d920d21cbfdd013
Davide Corsi <davide.corsi@par-tec.it>
2021-09-30 12:14:24 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+
+public class Main {
+	public static void main(String[] args) {
+		
+		// byte
+		byte b1 = -128;
+		byte b2 = 127;
+		
+		// short
+		short s1 = -32768;
+		short s2 = 32767;
+		
+		// int
+		int i1 = -2147483648;
+		int i2 = 2147483647;
+		
+		// long
+		long l1 = -9223372036854775808l;
+		long l2 = 9223372036854775807l;
+		
+		// float
+		float f1 = 3.14f;
+		
+		// double
+		double d1 = 3.14;
+		
+		// char
+		char c1 = 'a';
+		char c2 = '#';
+		char c3 = '.';
+		char c4 = 65; // 'A'
+		char c5 = -9;
+		char c6 = 0;
+		char c7 = 255;
+		
+		int i = 100 + 'a'; // 100 + 97
+		
+		// boolean
+		
+		boolean bool1 = true;
+		boolean bool2 = false;
+		boolean bool3 = 5 > 3;
+	}
+}
